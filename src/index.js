5444160a49fca349c2411760c39470d7a005ab32
= <=>
2022-09-22 10:07:43 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.Fragment>
     <GlobalStyles>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </GlobalStyles>
   </React.Fragment>
 );
